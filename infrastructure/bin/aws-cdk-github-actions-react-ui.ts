e3eb4df2acea4053a3427b2ab15fa62af7eb7b91
ttt <tschachtli.th@gmail.com>
2023-02-18 12:52:31 +0100

    cdk

--- infrastructure/bin/aws-cdk-github-actions-react-ui.ts
+++ infrastructure/bin/aws-cdk-github-actions-react-ui.ts
@@ -5,4 +5,5 @@ import { InfrastructureStack } from '../lib/aws-cdk-github-actions-react-ui-stac
 
 const app = new cdk.App();
 new InfrastructureStack(app, 'InfrastructureStack', {
+    env: { region: 'eu-central-1', account: '585098453895' }
 });
